d097f0d0a71f3d32499a4274f985237f7e8824ff
M.Murray <majoria@me.com>
2023-06-19 03:52:14 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Murray on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
