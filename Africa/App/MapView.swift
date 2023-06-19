3403a8fdb89034580d1f542ae8f716934e812578
M.Murray <majoria@me.com>
2023-06-19 05:02:03 -0700

    Added Assets and created launch screen

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Murray on 6/19/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
