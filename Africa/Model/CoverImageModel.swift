2731da73cb6b6b21c10fa3d8adf9ada8ddd5a44a
M.Murray <majoria@me.com>
2023-06-19 23:03:44 -0700

    Decode json file and fetch data with SwiftUI

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Murray on 6/19/23.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
